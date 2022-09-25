fd3929a1aa5dd98a16b6c5e1c3f76605cc622547
hannahallain <98673988+hannahallain@users.noreply.github.com>
2022-09-25 14:46:00 -0500

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -140,5 +140,11 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+
+  newFeature();
 };
 init();
+
+const newFeature = function () {
+  console.log('Welcome! New feature testing here');
+};
